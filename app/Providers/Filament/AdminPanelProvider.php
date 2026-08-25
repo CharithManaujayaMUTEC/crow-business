ae597970a7129d21dc424ce53555ea2dec6a8209
CharithManaujayaMUTEC <charithmanujaya1@gmail.com>
2026-08-26 01:56:13 +0530

    admin panel compact view enabled

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,15 +28,42 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogo(asset('images/crow-logo.png'))
             ->brandLogoHeight('2.5rem')
             ->brandName('Crow.lk Business Admin')
-            ->colors(['primary' => Color::Orange])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->middleware([
-                EncryptCookies::class, AddQueuedCookiesToResponse::class, StartSession::class,
-                AuthenticateSession::class, ShareErrorsFromSession::class, VerifyCsrfToken::class,
-                SubstituteBindings::class, DisableBladeIconComponents::class, DispatchServingFilamentEvent::class,
+
+            ->colors([
+                'primary' => Color::Orange,
             ])
-            ->authMiddleware([Authenticate::class]);
+
+            ->viteTheme('resources/css/filament/admin/theme.css')
+
+            ->discoverResources(
+                in: app_path('Filament/Resources'),
+                for: 'App\\Filament\\Resources'
+            )
+
+            ->discoverPages(
+                in: app_path('Filament/Pages'),
+                for: 'App\\Filament\\Pages'
+            )
+
+            ->discoverWidgets(
+                in: app_path('Filament/Widgets'),
+                for: 'App\\Filament\\Widgets'
+            )
+
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
     }
 }
